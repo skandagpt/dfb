d97727db67f9ab6df1e1423496ca1905b95c0e1b
Skandagpt <skandagpt@gmail.com>
2021-04-19 15:31:54 +0530

    mmessage Board commit

--- page/views.py
+++ page/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 # Create your views here.
-
-class HomePageView(TemplateView):
+from .models import Post
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
+    context_object_name='post_list'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
