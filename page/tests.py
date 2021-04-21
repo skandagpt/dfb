e5eceeb7cc7caa8678a2e4ec37cfaea9d187f44a
Skandagpt <skandagpt@gmail.com>
2021-04-21 11:55:29 +0530

    Added Test

--- page/tests.py
+++ page/tests.py
@@ -1,12 +1,37 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
 
-class SimpleTests(SimpleTestCase):
-    def test_home_page(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='just as test')
         
+    def test_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just as test')
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='another test')
+        
+    def test_view_url(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        
+    def test_view_url_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        
+    def test_templates(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response , 'home.html')
+        
+class SimpleTests(SimpleTestCase):
     def test_about_page(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
+
