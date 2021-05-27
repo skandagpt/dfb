220f9cb30655b99cf7053d0129ab1619d9d27afa
Skandagpt <skandagpt@gmail.com>
2021-05-27 12:08:26 +0530

    updated files

--- page/urls.py
+++ page/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView , AboutPageView
+from .views import HomePageView , AboutPageView , BlogDetailView
 
 urlpatterns = [
                 path('', HomePageView.as_view(), name='home'),
                 path('about/', AboutPageView.as_view(), name = 'about'),
+                path('post/<int:pk>/', BlogDetailView.as_view(), name='post-detail'),
             ]
--- page/views.py
+++ page/views.py
@@ -14,3 +14,4 @@ class AboutPageView(TemplateView):
 class BlogDetailView(DetailView):
     model=Post
     template_name= 'post-detail.html'
+    context_object_name='postd'
